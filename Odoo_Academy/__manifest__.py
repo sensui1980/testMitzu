97e7b0f84a79cfb1dea042244b9061bbf98830b0
David Gonzalez <raizen1980@hotmail.com>
2021-11-22 20:45:15 +0000

    [HEAD] Basic Module

---
+++ Odoo_Academy/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- encoding: utf-8 -*-
+
+
+{
+    'name': 'Odoo Academy',
+    
+    'summary': """Academy app to manage Training""",
+    
+    'description': """
+    Academy Module to manage Training:
+    -Courses
+    -Sessions
+    -Attendees   
+    """,
+    
+    'author': "David Gonzalez",
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    
+    'demo': [
+        
+    ],
+}
